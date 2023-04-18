cc41defe9ba3ae460cd36b7001f2f77976c99a1a
Ahmed Halim <ahmedabdelhalim1996@gmail.com>
2023-04-18 18:10:27 +0200

    [ADD] Setup: Adding react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path="/" element={<RootLayout />}>
+			<Route index element={<Home />} />
+		</Route>
+	)
+);
+
 function App() {
-	return <div className="App"></div>;
+	return <RouterProvider router={router} />;
 }
 
 export default App;
